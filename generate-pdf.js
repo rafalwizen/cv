79f660a94327090237a2ec2e172a1ba44f82fbb7
Rafal Wizen <rafal.wizen@gmail.com>
2026-05-01 12:18:47 +0200

    fix: restore Polish characters in PDF filename

--- generate-pdf.js
+++ generate-pdf.js
@@ -7,7 +7,7 @@ const path = require('path');
   const filePath = 'file:///' + path.resolve(__dirname, 'cv.html').replace(/\\/g, '/');
   await page.goto(filePath, { waitUntil: 'networkidle0' });
   await page.pdf({
-    path: path.join(__dirname, 'Rafal_Wizen_CV.pdf'),
+    path: path.join(__dirname, 'Rafał_Wiżeń_CV.pdf'),
     format: 'A4',
     printBackground: true,
     margin: { top: 0, right: 0, bottom: 0, left: 0 }
